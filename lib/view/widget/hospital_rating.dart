d572a308e5c234038377943b32640bc574035641
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-06-28 08:08:02 +0300

    refactor HospitalRateCard, HopitalsListViewBuilder.

---
+++ lib/view/widget/hospital_rating.dart
@@ -0,0 +1,52 @@
+import 'package:find_hospital/core/helper/extentions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../bloc/hospital_cubit/find_hospital_cubit.dart';
+
+class HospitalRateCard extends StatelessWidget {
+  const HospitalRateCard(
+      {super.key, required this.rating, required this.userTotalRating});
+  final double? rating;
+  final int? userTotalRating;
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = context.bloc<FindHospitalCubit>();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          children: [
+            Text(
+              "Rating",
+              style: context.textTheme.bodyMedium,
+            ),
+            SizedBox(height: 4.h),
+            Text(
+              rating.toString(),
+              style: context.textTheme.bodyMedium?.copyWith(
+                color: cubit.ratingChecker(rating: rating),
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Text(
+              "Users Ratings Total",
+              style: context.textTheme.bodyMedium,
+            ),
+            SizedBox(height: 4.h),
+            Text(
+              '$userTotalRating Users',
+              style: context.textTheme.bodyMedium?.copyWith(
+                color: cubit.ratingChecker(totalRating: userTotalRating),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/view/widget/hospitals_listview.dart
@@ -0,0 +1,52 @@
+
+import 'package:find_hospital/core/helper/extentions.dart';
+import 'package:flutter/material.dart';
+
+import '../../bloc/hospital_cubit/find_hospital_cubit.dart';
+import '../../core/constant/color.dart';
+import '../../core/constant/routes.dart';
+import '../../data/models/hospital_model.dart';
+
+class HopitalsListViewBuilder extends StatelessWidget {
+  const HopitalsListViewBuilder({
+    super.key,
+    required List<HospitalsPlaceInfo?> hospitalList,
+    required this.cubit,
+  }) : _hospitalList = hospitalList;
+
+  final List<HospitalsPlaceInfo?> _hospitalList;
+  final FindHospitalCubit cubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: _hospitalList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              onTap: () {
+                final HospitalsPlaceInfo? placeInfo = _hospitalList[index];
+                Navigator.pushNamed(context, RouteManager.details,
+                    arguments: placeInfo);
+              },
+              title: Text(
+                _hospitalList[index]?.name ?? '',
+                style: context.textTheme.bodyMedium,
+              ),
+              subtitle: Text(
+                cubit.isHospitalOpen(_hospitalList[index]?.openNow),
+                style: context.textTheme.bodyMedium?.copyWith(
+                    color: _hospitalList[index]?.openNow == true
+                        ? Colors.green
+                        : ColorManager.red),
+              ),
+              trailing: const Icon(Icons.chevron_right),
+              leading: const Icon(Icons.healing),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
